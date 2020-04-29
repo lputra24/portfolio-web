f98a6076648acdb2c9e720be6a7b1d85dd6608e4
lputra24 <lputra@student.unimelb.edu.au>
2020-04-29 17:49:04 +1000

    add burger menu animation

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,17 +5,27 @@ import logo from '../Resources/Logo(temp).png'
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="logo">
-          <a href="#"><img src={logo} alt="Leonardus Elbert"/></a>
-      </div>
-      <div className="nav">
-        <a href="#">Home</a>
-        <a href="#">Skills</a>
-        <a href="#">Project</a>
-        <a href="#">Contact</a>
-      </div>
-      <Navburger></Navburger>
+    <div>
+        <div className="navbar">
+            <div className="logo">
+                <a href="#"><img src={logo} alt="Leonardus Elbert"/></a>
+            </div>
+            <div className="nav">
+                <a href="#">Home</a>
+                <a href="#">Skills</a>
+                <a href="#">Project</a>
+                <a href="#">Contact</a>
+            </div>
+            <Navburger></Navburger>
+        </div>
+        <div className="navbar-mobile">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Project</a></li>
+                <li><a href="#">Contact</a> </li>
+            </ul>
+        </div>
     </div>
   );
 }
--- src/Components/navburger.js
+++ src/Components/navburger.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navburger.scss';
 
-function Navburger() {
-  return (
-    
-      <div className="nav-burger">
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-  );
+const Navburger = () => {
+    const [active,toggleActive] = useState(null);
+
+    return (
+        
+        <div className={`nav-burger ${active ? "" : "reverse"}`} 
+            onClick={()=>{active ? toggleActive(!active) : toggleActive(true)}}>
+            <div className={`line1${active==null ? " no-animation" : ""}`}></div>
+            <div className={`line2${active==null ? " no-animation" : ""}`}></div>
+            <div className={`line3${active==null ? " no-animation" : ""}`}></div>
+        </div>
+
+    );
 }
 
 export default Navburger;
